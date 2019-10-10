5dfb34a384dd78f33722fda95663fa5c35fc1382
20124899 <cdkgw11@hanmail.net>
2019-10-10 09:56:57 +0900

    흔한 개발자(17) 백업

---
+++ src/main/java/first/sample/controller/SampleController.java
@@ -0,0 +1,111 @@
+package first.sample.controller;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import egovframework.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
+import first.common.common.CommandMap;
+import first.sample.service.SampleService;
+
+@Controller
+public class SampleController {
+	Logger log = Logger.getLogger(this.getClass());
+
+	/*
+	 * @RequestMapping(value = "/sample/openSampleList.do") public ModelAndView
+	 * openSampleList(Map<String, Object> commandMap) throws Exception {
+	 * ModelAndView mv = new ModelAndView(""); log.debug("인터셉터 테스트"); return mv;
+	 * }
+	 */
+
+	@Resource(name = "sampleService")
+	private SampleService sampleService;
+
+	@RequestMapping(value="/sample/openBoardList.do")
+	public ModelAndView openBoardList(CommandMap commandMap) throws Exception{
+	     
+	    ModelAndView mv = new ModelAndView("sample/boardList");
+	    Map<String,Object> map = sampleService.selectBoardList(commandMap.getMap());
+	    mv.addObject("list", map.get("result"));
+	    mv.addObject("paginationInfo", (PaginationInfo)map.get("paginationInfo"));
+	     
+	    return mv;
+	}
+
+	@RequestMapping(value = "/sample/testMapArgumentResolver.do")
+	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("");
+		if (commandMap.isEmpty() == false) {
+			Iterator<Entry<String, Object>> iterator = commandMap.getMap().entrySet().iterator();
+			Entry<String, Object> entry = null;
+			while (iterator.hasNext()) {
+				entry = iterator.next();
+				log.debug("key : " + entry.getKey() + ", value : " + entry.getValue());
+			}
+		}
+		return mv;
+	}
+
+	// 입력 페이지로
+	@RequestMapping(value = "/sample/openBoardWrite.do")
+	public ModelAndView openBoardWrite(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/sample/boardWrite");
+		return mv;
+	}
+
+	// DB 글쓰기 기능
+	@RequestMapping(value = "/sample/insertBoard.do")
+	public ModelAndView insertBoard(CommandMap commandMap, HttpServletRequest request) throws Exception {
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
+		sampleService.insertBoard(commandMap.getMap(), request);
+		return mv;
+	}
+
+	// 상세화면 보기
+	@RequestMapping(value = "/sample/openBoardDetail.do")
+	public ModelAndView openBoardDetail(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/sample/boardDetail");
+		Map<String, Object> map = sampleService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map.get("map"));
+		mv.addObject("list", map.get("list"));
+		return mv;
+	}
+
+	// 수정 페이지로
+	@RequestMapping(value = "/sample/openBoardUpdate.do")
+	public ModelAndView openBoardUpdate(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("/sample/boardUpdate");
+		Map<String, Object> map = sampleService.selectBoardDetail(commandMap.getMap());
+		mv.addObject("map", map.get("map"));
+		mv.addObject("list", map.get("list"));
+		return mv;
+	}
+
+	// DB 수정 기능
+	@RequestMapping(value = "/sample/updateBoard.do")
+	public ModelAndView updateBoard(CommandMap commandMap, HttpServletRequest request) throws Exception {
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardDetail.do");
+		sampleService.updateBoard(commandMap.getMap(), request);
+		mv.addObject("IDX", commandMap.get("IDX"));
+		return mv;
+	}
+
+	// 글 삭제
+	@RequestMapping(value = "/sample/deleteBoard.do")
+	public ModelAndView deleteBoard(CommandMap commandMap) throws Exception {
+		ModelAndView mv = new ModelAndView("redirect:/sample/openBoardList.do");
+		sampleService.deleteBoard(commandMap.getMap());
+		return mv;
+	}
+
+}
